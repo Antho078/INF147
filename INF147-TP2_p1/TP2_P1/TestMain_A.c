2da2f9aaa7503b2de9671625a7e1876a7c506291
powerlemming <47431393+powerlemming@users.noreply.github.com>
2025-02-16 15:48:02 -0500

    Push po pire partie 1
    - Tous mis ensemble
    - Fait main de test
    - Debatter changer_plaque()

--- INF147-TP2_p1/TP2_P1/TestMain_A.c
+++ INF147-TP2_p1/TP2_P1/TestMain_A.c
@@ -1,1 +1,97 @@
 #include "plaque.h"
+#include <conio.h>
+
+// Définition des codes des touches direction et de la touche ESC
+typedef enum
+{
+	KEY_UP = 72,
+	KEY_DOWN = 80,
+	KEY_RIGHT = 77,
+	KEY_LEFT = 75,
+	KEY_ESC = 27
+} t_codes_touches;
+
+int main(void)
+{
+	int py0; // Position verticale de la case vide
+	int px0; // Position horizontale de la case vide
+
+	int posy; // Position verticale trouvée par trouver_numero()
+	int posx; // Position horizontale trouvée par trouver_numero()
+
+	t_codes_touches touche; // Variable pour stocker la touche pressée
+
+	t_plaque plaque_test; // Déclaration de la plaque de jeu
+
+	int cout = 0;	// Coût de l'état actuel de la plaque
+	int changement; // Indicateur de changement après un déplacement
+
+	// Copie de la solution dans plaque_test comme plaque de depart
+	memcpy(plaque_test, SOLUTION, sizeof(t_plaque));
+
+	printf("Plaque solution\r\n");
+	afficher_plaque(plaque_test);
+
+	// Mélange de la plaque en effectuant des déplacements aléatoires
+	melanger_plaque(plaque_test, &py0, &px0);
+
+	printf("Plaque melangee\r\n");
+	afficher_plaque(plaque_test);
+
+	// Boucle infinie pour la gestion des déplacements de l'utilisateur
+	while (1)
+	{
+		touche = 0; // Réinitialisation de la touche pressée
+		if (_kbhit())
+		{					   // Vérifie si une touche a été pressée
+			touche = _getch(); // Lecture de la touche pressée
+			if (touche == 0 || touche == 224)
+			{					   // Vérifie si la touche est un ASCII etendu
+				touche = _getch(); // Lecture du deuxième code de la touche
+				switch (touche)
+				{
+				case KEY_LEFT: // Déplacement vers la gauche
+					changement = changer_plaque(plaque_test, GAUCHE, &py0, &px0);
+					break;
+				case KEY_RIGHT: // Déplacement vers la droite
+					changement = changer_plaque(plaque_test, DROITE, &py0, &px0);
+					break;
+				case KEY_UP: // Déplacement vers le haut
+					changement = changer_plaque(plaque_test, HAUT, &py0, &px0);
+					break;
+				case KEY_DOWN: // Déplacement vers le bas
+					changement = changer_plaque(plaque_test, BAS, &py0, &px0);
+					break;
+				default:
+					changement = 0; // Aucun déplacement effectué
+					break;
+				}
+
+				// Si un déplacement a été effectué avec succès
+				if (changement)
+				{
+					system("cls");											 // Efface l'écran
+					afficher_plaque(plaque_test);							 // Affiche la plaque mise à jour
+					printf("Cout: %d\r\n", evaluer_plaque(plaque_test));	 // Affiche le coût actuel
+					printf("CASE VIDE est a la position [%d,%d]", py0, px0); // Indique la nouvelle position de la case vide
+				}
+				else
+				{
+					printf("\r\nERREUR DIRECTION"); // Affichage d'une erreur si le déplacement est invalide
+				}
+			}
+			else
+			{
+				// Vérifie si l'utilisateur a appuyé sur Échap pour quitter
+				if (touche == KEY_ESC)
+					break; // Quitte la boucle
+			}
+		}
+	}
+
+	// Vérification de la position de la case vide avec trouver_numero()
+	trouver_numero(plaque_test, 0, &posy, &posx);
+	printf("\r\nEt Selon trouver_numero() a [%d,%d]", posy, posx);
+
+	return 1; // Fin du programme
+}
--- INF147-TP2_p1/TP2_P1/plaque.h
+++ INF147-TP2_p1/TP2_P1/plaque.h
@@ -1,26 +1,106 @@
 #pragma once
-#define  DIM   3            // la largeur de la plaque (3x3 cases en tout)
-#define  VIDE  0            // la valeur de la case vide 
-#define  NBCOUPS  200  // le nombre de coups à faire pour mélanger la plaque initiale
 
-/* Type énuméré pour les 4 déplacements possibles */
-typedef enum { NULLE = -1, HAUT, GAUCHE, BAS, DROITE } t_direction;
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define DIM 3       // la largeur de la plaque (3x3 cases en tout)
+#define VIDE 0      // la valeur de la case vide
+#define NBCOUPS 200 // le nombre de coups Ã  faire pour mÃ©langer la plaque initiale
+
+/* Type Ã©numÃ©rÃ© pour les 4 dÃ©placements possibles */
+typedef enum
+{
+   NULLE = -1,
+   HAUT,
+   GAUCHE,
+   BAS,
+   DROITE
+} t_direction;
 /* Type-tableau pour la plaque de jeu */
 typedef int t_plaque[DIM][DIM];
-/* Constante qui illustre la plaque-solution à trouver */
-static const t_plaque SOLUTION = { {1, 2, 3}, {4, 5, 6}, {7, 8, VIDE} };
+/* Constante qui illustre la plaque-solution Ã  trouver */
+static const t_plaque SOLUTION = {{1, 2, 3}, {4, 5, 6}, {7, 8, VIDE}};
 
-//Cette fonction recherche la position(ligne et colonne) du numéro « num » dans la plaque de jeu pla.Elle met à jour les références posy et posx avec les indices de la ligne et de la colonne où se trouve ce numéro.NOTE : Cette fonction suppose que le numéro num existe dans la plaque.
-void trouver_numero(const t_plaque pla, int num, int* posy, int* posx);
+/*===========================================================================*/
+/* DÃ©claration des fonctions publiques */
+/*===========================================================================*/
 
-//Cette fonction reçoit une plaque de jeu, une direction valide ainsi que la position[py_0, px_0] de la case vide.Si le déplacement demandé est valide – ne déplacera pas la case vide en dehors des limites de la plaque – la fonction effectuera le déplacement de la case vide dans la direction voulue, va mettre à jour la position[py_0, px_0] de cette case et va renvoyer 1.  Sinon, elle renvoie 0 et la plaque restera inchangée.
-int changer_plaque(t_plaque pla, t_direction dir, int* py_0, int* px_0);
+/*===========================================================================*/
+/*
+   Fonction    : trouver_numero()
+   Description : Recherche la position (ligne et colonne) du numÃ©ro Â« num Â»
+                 dans la plaque de jeu pla. Met Ã  jour les rÃ©fÃ©rences posy et
+                 posx avec les indices de la ligne et de la colonne oÃ¹ se
+                 trouve ce numÃ©ro.
 
-//Cette fonction commence par copier l’état - solution dans la plaque reçue en paramètre.Ensuite elle va effectuer « NBCOUPS » déplacements aléatoires de la case vide pour bien mélanger la plaque de départ.Assurez - vous que la position[py_0, px_0] de la case vide a bien été initialisée après avoir mélangée la plaque.
-void melanger_plaque(t_plaque pla, int* py_0, int* px_0);
+   ParamÃ¨tres  : - pla  : La plaque dans laquelle on cherche le numÃ©ro
+                 - num  : Le numÃ©ro recherchÃ©
+                 - posy : Pointeur pour stocker la position verticale
+                 - posx : Pointeur pour stocker la position horizontale
 
-//Cette fonction sera utile pour l’algorithme de recherche de la solution(2ième partie du TP).Elle évalue le « coût minimal estimé » pour atteindre l’état - solution à partir de la plaque reçue en paramètre.Le « coût minimal estimé » est simplement la somme des distances verticales / horizontales qui séparent chaque numéro(ou chaque case non - vide) de sa position finale désirée(dans la plaque - solution).Voici la formule du coût minimal estimé :
-int  evaluer_plaque(const t_plaque pla);
+   Retour      : Aucun (les positions sont mises Ã  jour via les pointeurs)
+*/
+/*===========================================================================*/
+void trouver_numero(const t_plaque pla, int num, int *posy, int *posx);
 
-//Fonction qui affiche simplement la plaque reçue à l’écran, « DIM » valeurs par ligne.
+/*===========================================================================*/
+/*
+   Fonction    : changer_plaque()
+   Description : DÃ©place la case vide dans la direction indiquÃ©e si le
+                 dÃ©placement est valide.
+
+   ParamÃ¨tres  : - pla  : La plaque de jeu
+                 - dir  : La direction du dÃ©placement
+                 - py_0 : Pointeur vers la position verticale de la case vide
+                 - px_0 : Pointeur vers la position horizontale de la case vide
+
+   Retour      : - 1 si le dÃ©placement est valide et effectuÃ©
+                 - 0 sinon (aucun changement dans la plaque)
+*/
+/*===========================================================================*/
+int changer_plaque(t_plaque pla, t_direction dir, int *py_0, int *px_0);
+
+/*===========================================================================*/
+/*
+   Fonction    : melanger_plaque()
+   Description : Initialise la plaque en copiant lâ€™Ã©tat-solution, puis effectue
+                 un nombre dÃ©fini de dÃ©placements alÃ©atoires de la case vide
+                 pour mÃ©langer la plaque.
+
+   ParamÃ¨tres  : - pla  : La plaque de jeu Ã  mÃ©langer
+                 - py_0 : Pointeur vers la position verticale de la case vide
+                 - px_0 : Pointeur vers la position horizontale de la case vide
+
+   Retour      : Aucun (modifie la plaque en place)
+*/
+/*===========================================================================*/
+void melanger_plaque(t_plaque pla, int *py_0, int *px_0);
+
+/*===========================================================================*/
+/*
+   Fonction    : evaluer_plaque()
+   Description : Calcule le coÃ»t minimal estimÃ© pour atteindre lâ€™Ã©tat-solution
+                 Ã  partir de la plaque reÃ§ue en paramÃ¨tre. Le coÃ»t est basÃ©
+                 sur la somme des distances verticales et horizontales entre
+                 chaque numÃ©ro et sa position finale dÃ©sirÃ©e.
+
+   ParamÃ¨tres  : - pla : La plaque de jeu Ã  Ã©valuer
+
+   Retour      : - Le coÃ»t minimal estimÃ©
+*/
+/*===========================================================================*/
+int evaluer_plaque(const t_plaque pla);
+
+/*===========================================================================*/
+/*
+   Fonction    : afficher_plaque()
+   Description : Affiche la plaque reÃ§ue Ã  lâ€™Ã©cran, avec Â« DIM Â» valeurs par ligne.
+
+   ParamÃ¨tres  : - pla : La plaque Ã  afficher
+
+   Retour      : Aucun (affichage sur la sortie standard)
+*/
+/*===========================================================================*/
 void afficher_plaque(const t_plaque pla);
